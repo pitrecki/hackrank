e92aff266e037013e8cd63a859efbaaa9c141200
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-01-28 11:17:05 +0100

    Add private field in SortBigDecimalDescendingTest

--- src/test/java/org/pitrecki/hackrank/bignumber/medium/SortBigDecimalDescendingTest.java
+++ src/test/java/org/pitrecki/hackrank/bignumber/medium/SortBigDecimalDescendingTest.java
@@ -13,16 +13,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SortBigDecimalDescendingTest
 {
     private SortBigDecimalDescending sortBigDecimalDescending;
+    private String[] arrayOfNumbers;
 
     @BeforeEach
     void setUp() {
         this.sortBigDecimalDescending = new SortBigDecimalDescending();
+        this.arrayOfNumbers = new String[]{"9", "-100", "50", "0", "56.6", "90", "0.12", ".12", "02.34", "000.000"};
     }
 
     @Test
     @DisplayName("Check correct descend order of entered array")
     void teestShouldReturnNewArrayWithRevereseOrderedNumbers() {
-        String[] arrayOfNumbers = {"9", "-100", "50", "0", "56.6", "90", "0.12", ".12", "02.34", "000.000"};
         String[] expected = {"90", "56.6", "50", "9", "02.34", "0.12", ".12", "0", "000.000", "-100"};
 
         String[] actual = sortBigDecimalDescending.sortBigDecimalArray(arrayOfNumbers);
